d207af9cf6d4407a9c4982be8d15e38b108d1ed3
Kevin Malone <malonek1@students.rowan.edu>
2021-08-09 00:54:45 -0400

    Added basic quotes feature

--- quote.py
+++ quote.py
@@ -1,6 +1,11 @@
 import discord
 import os
+import random
 
+quotes = []
+
+def addQuote(quote):
+    quotes.append(quote)
 
 client = discord.Client()
 
@@ -13,7 +18,20 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$quote'):
+    if message.content.startswith('$cry'):
         await message.channel.send('This is a cry for help!')
+
+    if message.content.startswith('$add'):
+        await message.channel.send('Enter your quote here:')
+
+        def check(msg):
+            return msg.author == message.author and msg.channel == message.channel
+
+        msg = await client.wait_for("message", check=check)
+        addQuote(msg.content)
+        await message.channel.send('Quote added!')
+
+    if message.content.startswith('$quote'):
+        await message.channel.send('"' + random.choice(quotes) + '"')
 
 client.run(os.getenv('TOKEN'))
