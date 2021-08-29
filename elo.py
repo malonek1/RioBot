50742cce68732a9d8ce400685a265e0b33efe901
Kevin Malone <malonek1@students.rowan.edu>
2021-08-29 18:19:49 -0400

    Added error checking for scores out of bounds

--- elo.py
+++ elo.py
@@ -20,31 +20,35 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def submit(ctx, winnerScore: int, loserScore: int, loserUser: discord.Member):
-    winnerUser = ctx.author
-    embed=discord.Embed(title= f'{loserUser.name} confirm these results by reacting with :white_check_mark: or reject the results with :x: ', color=0xC496EF)
-    embed.add_field(name= f'{winnerUser.name}:', value= winnerScore, inline=False)
-    embed.add_field(name= f'{loserUser.name}:', value= loserScore, inline=True)
-    botReaction = await ctx.send(embed=embed)
-    checkEmoji = "\U00002705"
-    exEmoji = "\U0000274C"
-    await botReaction.add_reaction(checkEmoji)
-    await botReaction.add_reaction(exEmoji)
-
-    def checkConfirm(reaction, user):
-        return user == loserUser and (str(reaction.emoji) in [checkEmoji] or str(reaction.emoji) in [exEmoji])
-
-    try:
-        reaction, user = await bot.wait_for('reaction_add', timeout=10.0, check=checkConfirm)
-    except asyncio.TimeoutError:
-        await botReaction.delete()
-        embed=discord.Embed(title= 'Cancelled match between ' + f'{winnerUser.name}' + ' and ' + f'{loserUser.name}' + ' for not reacting in time!' , color=0xFF5733)
+    if (winnerScore < 0) or (loserScore < 0) or (winnerScore > 99) or (loserScore > 99):
+        embed=discord.Embed(title= 'Scores must be between 0 and 100!', color=0xEA7D07)
         await ctx.send(embed=embed)
     else:
-        if reaction.emoji == checkEmoji:
-            embed=discord.Embed(title= 'Confirmed match between ' + f'{winnerUser.name}' + ' and ' + f'{loserUser.name}' + '!' , color=0x138F13)
+        winnerUser = ctx.author
+        embed=discord.Embed(title= f'{loserUser.name} confirm these results by reacting with :white_check_mark: or reject the results with :x: ', color=0xC496EF)
+        embed.add_field(name= f'{winnerUser.name}:', value= winnerScore, inline=False)
+        embed.add_field(name= f'{loserUser.name}:', value= loserScore, inline=True)
+        botReaction = await ctx.send(embed=embed)
+        checkEmoji = "\U00002705"
+        exEmoji = "\U0000274C"
+        await botReaction.add_reaction(checkEmoji)
+        await botReaction.add_reaction(exEmoji)
+
+        def checkConfirm(reaction, user):
+            return user == loserUser and (str(reaction.emoji) in [checkEmoji] or str(reaction.emoji) in [exEmoji])
+
+        try:
+            reaction, user = await bot.wait_for('reaction_add', timeout=10.0, check=checkConfirm)
+        except asyncio.TimeoutError:
+            await botReaction.delete()
+            embed=discord.Embed(title= 'Cancelled match between ' + f'{winnerUser.name}' + ' and ' + f'{loserUser.name}' + ' for not reacting in time!' , color=0xFF5733)
             await ctx.send(embed=embed)
-        elif reaction.emoji == exEmoji:
-            embed=discord.Embed(title= 'Cancelled match between ' + f'{winnerUser.name}' + ' and ' + f'{loserUser.name}' + '!' , color=0xFF5733)
-            await ctx.send(embed=embed)
+        else:
+            if reaction.emoji == checkEmoji:
+                embed=discord.Embed(title= 'Confirmed match between ' + f'{winnerUser.name}' + ' and ' + f'{loserUser.name}' + '!' , color=0x138F13)
+                await ctx.send(embed=embed)
+            elif reaction.emoji == exEmoji:
+                embed=discord.Embed(title= 'Cancelled match between ' + f'{winnerUser.name}' + ' and ' + f'{loserUser.name}' + '!' , color=0xFF5733)
+                await ctx.send(embed=embed)
 
 bot.run(os.getenv('TOKEN'))
