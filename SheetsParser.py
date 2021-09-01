2a3e09d64ecaad2f1ca7a700e1e7c5ed00b9a2cb
Kevin Malone <malonek1@students.rowan.edu>
2021-09-01 00:15:34 -0400

    Added functionality to populate game stats in sheets for an existing user in the spreadsheet

--- SheetsParser.py
+++ SheetsParser.py
@@ -11,10 +11,36 @@ class SheetsParser:
 
     def confirmMatch(self, winnerName, loserName, winnerScore, loserScore):
         print('function: confirmMatch entered')
+
+        winnerCell = sheet.find(winnerName)
+        loserCell = sheet.find(loserName)
+
         #Update Winner
-        sheet.update_cell(2, 4, winnerName)
-        sheet.update_cell(2, 5, '1')
+        if winnerCell:
+            print('--------------')
+            print('Winner exists!')
+            print('Row: ' + f'{winnerCell.row}')
+            print('Column: ' + f'{winnerCell.col}')
+            print('Value: ' + f'{winnerCell.value}')
+            print('Player Wins: ' + f'{sheet.cell(winnerCell.row, winnerCell.col + 1).value}')
+            print('Total Games: ' + f'{sheet.cell(winnerCell.row, winnerCell.col + 4).value}')
+
+            playerWins = sheet.cell(winnerCell.row, winnerCell.col + 1).value
+            totalGames = sheet.cell(winnerCell.row, winnerCell.col + 4).value
+            sheet.update_cell(winnerCell.row, winnerCell.col + 1, int(playerWins) + 1)
+            sheet.update_cell(winnerCell.row, winnerCell.col + 4, int(totalGames) + 1)
 
         #Update Loser
-        sheet.update_cell(3, 4, loserName)
-        sheet.update_cell(3, 6, '1')
+        if loserCell:
+            print('-------------')
+            print('Loser exists!')
+            print('Row: ' + f'{loserCell.row}')
+            print('Column: ' + f'{loserCell.col}')
+            print('Value: ' + f'{loserCell.value}')
+            print('Player Losses: ' + f'{sheet.cell(loserCell.row, loserCell.col + 2).value}')
+            print('Total Games: ' + f'{sheet.cell(loserCell.row, loserCell.col + 4).value}')
+
+            playerLosses = sheet.cell(loserCell.row, loserCell.col + 2).value
+            totalGames = sheet.cell(loserCell.row, loserCell.col + 4).value
+            sheet.update_cell(loserCell.row, loserCell.col + 2, int(playerLosses) + 1)
+            sheet.update_cell(loserCell.row, loserCell.col + 4, int(totalGames) + 1)
