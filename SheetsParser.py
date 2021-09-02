81e75717f4abfc5c740bdd97b8866d85754d5570
Kevin Malone <malonek1@students.rowan.edu>
2021-09-02 00:24:57 -0400

    Updated confirmMatch command to add winner, loser, scores, and indexes to a logging sheet

--- SheetsParser.py
+++ SheetsParser.py
@@ -1,20 +1,33 @@
-#Google Sheets imports:
+#Google eloSheets imports:
 import gspread
 
 #Creation of gspread object
 client = gspread.service_account()
-sheet = client.open('MSSB Elo').sheet1
+eloSheet = client.open('MSSB Elo').worksheet('Elo')
+logSheet = client.open('MSSB Elo').worksheet('Logs')
 
-class SheetsParser:
+class EloSheetsParser:
     def __init__(self, name):
         self.name = name
 
+    def next_available_row(self, worksheet):
+        str_list = list(filter(None, worksheet.col_values(1)))
+        return str(len(str_list)+1)
+
     def confirmMatch(self, winnerName, loserName, winnerScore, loserScore):
         print('function: confirmMatch entered')
+        nextRow = self.next_available_row(logSheet)
+        print('next row index: ' + f'{int(nextRow) - 1}')
 
-        winnerCell = sheet.find(winnerName)
-        loserCell = sheet.find(loserName)
+        logSheet.update_acell("A{}".format(nextRow), f'{int(nextRow) - 1}')
+        logSheet.update_acell("C{}".format(nextRow), winnerName)
+        logSheet.update_acell("D{}".format(nextRow), winnerScore)
+        logSheet.update_acell("F{}".format(nextRow), loserName)
+        logSheet.update_acell("G{}".format(nextRow), loserScore)
 
+    def playerStats(self, winnerName, loserName):
+        winnerCell = eloSheet.find(winnerName)
+        loserCell = eloSheet.find(loserName)       
         #Update Winner
         if winnerCell:
             print('--------------')
@@ -22,13 +35,10 @@ class SheetsParser:
             print('Row: ' + f'{winnerCell.row}')
             print('Column: ' + f'{winnerCell.col}')
             print('Value: ' + f'{winnerCell.value}')
-            print('Player Wins: ' + f'{sheet.cell(winnerCell.row, winnerCell.col + 1).value}')
-            print('Total Games: ' + f'{sheet.cell(winnerCell.row, winnerCell.col + 4).value}')
+            print('Player Wins: ' + f'{eloSheet.cell(winnerCell.row, winnerCell.col + 1).value}')
 
-            playerWins = sheet.cell(winnerCell.row, winnerCell.col + 1).value
-            totalGames = sheet.cell(winnerCell.row, winnerCell.col + 4).value
-            sheet.update_cell(winnerCell.row, winnerCell.col + 1, int(playerWins) + 1)
-            sheet.update_cell(winnerCell.row, winnerCell.col + 4, int(totalGames) + 1)
+            playerWins = eloSheet.cell(winnerCell.row, winnerCell.col + 1).value
+            eloSheet.update_cell(winnerCell.row, winnerCell.col + 1, int(playerWins) + 1)
 
         #Update Loser
         if loserCell:
@@ -37,10 +47,7 @@ class SheetsParser:
             print('Row: ' + f'{loserCell.row}')
             print('Column: ' + f'{loserCell.col}')
             print('Value: ' + f'{loserCell.value}')
-            print('Player Losses: ' + f'{sheet.cell(loserCell.row, loserCell.col + 2).value}')
-            print('Total Games: ' + f'{sheet.cell(loserCell.row, loserCell.col + 4).value}')
+            print('Player Losses: ' + f'{eloSheet.cell(loserCell.row, loserCell.col + 2).value}')
 
-            playerLosses = sheet.cell(loserCell.row, loserCell.col + 2).value
-            totalGames = sheet.cell(loserCell.row, loserCell.col + 4).value
-            sheet.update_cell(loserCell.row, loserCell.col + 2, int(playerLosses) + 1)
-            sheet.update_cell(loserCell.row, loserCell.col + 4, int(totalGames) + 1)
+            playerLosses = eloSheet.cell(loserCell.row, loserCell.col + 2).value
+            eloSheet.update_cell(loserCell.row, loserCell.col + 2, int(playerLosses) + 1)
