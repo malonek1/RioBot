5051f0878afa209566c558450dadc8b873102be4
Isadora Anemonae <IsadoraAnemonae@gmail.com>
2022-12-11 12:22:43 -0600

    Randomize Commands
    Bulk of update that adds randomize commands

---
+++ cogs/randomize_commands.py
@@ -0,0 +1,157 @@
+from io import BytesIO
+
+import discord
+from discord.ext import commands
+
+from helpers.image_builder import buildTeamImageHighlightCaptain
+from services.random_functions import *
+from helpers.team_sorter import sortTeamsByTier
+
+# Random Cog Properties
+hex_y = 0xE8E337        # Error message
+hex_r = 0xF46D75        # Success message
+
+NL = "\n"
+random_help_string = "Here is a list of random commands you can use and their functions:" + NL \
+                + "`!random teams` - Generate two random teams with no dupes but random variants" + NL \
+                + "`!random teams dupes` - Generate two random teams with dupes enabled" + NL \
+                + "`!random teams balanced` - Generate two broadly balanced teams" + NL \
+                + "`!random teams power` - Generate two teams with top tier characters and distributed pitching" + NL \
+                + "`!random stadium` - Returns a random stadium" + NL \
+                + "`!random character` - Returns a random character" + NL \
+                + "`!random mode` - Returns a random game type for you to play" + NL \
+                + "`!pick options...` - Picks an option randomly" + NL \
+                + "`!pickmany N options...` - Picks N options randomly" + NL \
+                + "`!shuffle options...` - Shuffles options and returns them" + NL \
+                + "`!coin` - Flip a coin" + NL \
+                + "`!roll N` - Roll a die of N sides"
+
+
+# Cog Class
+class RandomizeCommands(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(name="random", help="Random Baseball Functions. Please use `!random help` for more info")
+    @commands.cooldown(1, 15, commands.BucketType.user)
+    async def random(self, ctx, command, qualifier=""):
+        command = command.lower()
+        qualifier = qualifier.lower()
+        if "help" in command:
+            embed = discord.Embed(title="Random Commands Help", description=random_help_string)
+            await ctx.send(embed=embed)
+        elif command == "character" or command == "chara" or command == "char":
+            embed = discord.Embed(title=rfRandomCharacter(), color=hex_r)
+            await ctx.send(embed=embed)
+        elif command == "stadium":
+            embed = discord.Embed(title=rfRandomStadium(), color=hex_r)
+            await ctx.send(embed=embed)
+        elif command == "mode" or command == "modes":
+            embed = discord.Embed(title=rfRandomMode(), color=hex_r)
+            await ctx.send(embed=embed)
+        elif command == "teams":
+            if not qualifier or qualifier == "":
+                team_list = rfRandomTeamsWithoutDupes()
+                title = "**Random Teams**"
+                description = "Pure random teams without dupes but random variants. Captains are highlighted."
+            elif "dupes" in qualifier:
+                team_list = rfRandomTeamsWithDupes()
+                title = "**Random Teams with Dupes** "
+                description = "Pure random teams with duplicates enabled. Captains are highlighted."
+            elif "balance" in qualifier:
+                team_list = rfRandomBalancedTeams()
+                title = "**Random Balanced Teams**"
+                description = "Random teams where each team is given a character from one of five broadly balanced " \
+                              "tiers until teams are filled. Captains are highlighted."
+            elif "power" in qualifier:
+                team_list = rfRandomPowerTeams()
+                title = "**Random Power Teams**"
+                description = "Random teams made from top characters exclusively. Duplicates are enabled. Each team " \
+                              "is guaranteed one meta pitcher. Captains are highlighted. "
+
+            captain_list = [team_list[0][0], team_list[1][0]]
+            team_list = sortTeamsByTier(team_list)
+
+            teams_image = buildTeamImageHighlightCaptain(team_list, captain_list)
+
+            with BytesIO() as image_binary:
+                teams_image.save(image_binary, 'PNG')
+                image_binary.seek(0)
+
+                file = discord.File(fp=image_binary, filename='image.png')
+                embed = discord.Embed(title=title, description=description, color=hex_r)
+                embed.set_image(url="attachment://image.png")
+                await ctx.send(file=file, embed=embed)
+
+                # await ctx.send(title, file=discord.File(fp=image_binary, filename='image.png'))
+
+        else:
+            embed = discord.Embed(description="Alas poor random command; I do not know thee well", color=hex_y)
+            await ctx.send(embed=embed)
+    # End random
+
+    @commands.command(name="pick", help="Pick one item from a list")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def pick(self, ctx, *arg):
+        if len(arg) > 1:
+            embed = discord.Embed(description=rfPickOne(arg), color=hex_r)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(description="Please give me more options than that", color=hex_y)
+            await ctx.send(embed=embed)
+    # End pick
+
+    @commands.command(name="pickmany", help="Pick N items from a list")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def pick_many(self, ctx, choices, *arg):
+        try:
+            pick_number = int(choices)
+            if len(arg) > 0 and pick_number < len(arg):
+                embed = discord.Embed(description=rfPickMany(pick_number, arg), color=hex_r)
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(description="I don't think you quite know what you're asking", color=hex_y)
+                await ctx.send(embed=embed)
+        except ValueError:
+            embed = discord.Embed(description="Give me a number of choices to make. For example: `!pickmany 2 red "
+                                              "blue green`", color=hex_y)
+            await ctx.send(embed=embed)
+    # End pick many
+
+    @commands.command(name="shuffle", help="Shuffle the order of a list")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def shuffle(self, ctx, *arg):
+        if len(arg) > 1:
+            embed = discord.Embed(description=rfShuffle(arg), color=hex_r)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(description="No sense in shuffling that", color=hex_y)
+            await ctx.send(embed=embed)
+    # End shuffle
+
+    @commands.command(name="coin", help="Toss a coin to your witcher")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def coin(self, ctx):
+        embed = discord.Embed(title=rfFlipCoin(), color=hex_r)
+        await ctx.send(embed=embed)
+    # End coin
+
+    @commands.command(name="roll", help="Roll an N-sided die")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def roll(self, ctx, die):
+        try:
+            die_sides = int(die)
+            if die_sides > 2:
+                embed = discord.Embed(title=rfRollDice(die_sides), color=hex_r)
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(description="I can't roll that in this dimension", color=hex_y)
+                await ctx.send(embed=embed)
+        except ValueError:
+            embed = discord.Embed(description="That isn't a dice", color=hex_y)
+            await ctx.send(embed=embed)
+    # End roll
+
+
+async def setup(client):
+    await client.add_cog(RandomizeCommands(client))
--- resources/characters.py
+++ resources/characters.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 # Character ID mappings
 mappings = {
     0: "Mario",
@@ -322,6 +324,65 @@ aliases = {
     "b!bro": 53,
     "bbro": 53
 }
+
+
+# Character Enums
+class Char(Enum):
+    MARIO = "Mario"
+    LUIGI = "Luigi"
+    DK = "DK"
+    DIDDY = "Diddy"
+    PEACH = "Peach"
+    DAISY = "Daisy"
+    YOSHI = "Yoshi"
+    BABY_MARIO = "Baby Mario"
+    BABY_LUIGI = "Baby Luigi"
+    BOWSER = "Bowser"
+    WARIO = "Wario"
+    WALUIGI = "Waluigi"
+    KOOPA_R = "Koopa(R)"
+    TOAD_R = "Toad(R)"
+    BOO = "Boo"
+    TOADETTE = "Toadette"
+    SHY_GUY_R = "Shy Guy(R)"
+    BIRDO = "Birdo"
+    MONTY = "Monty"
+    BOWSER_JR = "Bowser Jr"
+    PARATROOPA_R = "Paratroopa(R)"
+    PIANTA_B = "Pianta(B)"
+    PIANTA_R = "Pianta(R)"
+    PIANTA_Y = "Pianta(Y)"
+    NOKI_B = "Noki(B)"
+    NOKI_R = "Noki(R)"
+    NOKI_G = "Noki(G)"
+    BRO_H = "Bro(H)"
+    TOADSWORTH = "Toadsworth"
+    TOAD_B = "Toad(B)"
+    TOAD_Y = "Toad(Y)"
+    TOAD_G = "Toad(G)"
+    TOAD_P = "Toad(P)"
+    MAGIKOOPA_B = "Magikoopa(B)"
+    MAGIKOOPA_R = "Magikoopa(R)"
+    MAGIKOOPA_G = "Magikoopa(G)"
+    MAGIKOOPA_Y = "Magikoopa(Y)"
+    KING_BOO = "King Boo"
+    PETEY = "Petey"
+    DIXIE = "Dixie"
+    GOOMBA = "Goomba"
+    PARAGOOMBA = "Paragoomba"
+    KOOPA_G = "Koopa(G)"
+    PARATROOPA_G = "Paratroopa(G)"
+    SHY_GUY_B = "Shy Guy(B)"
+    SHY_GUY_Y = "Shy Guy(Y)"
+    SHY_GUY_G = "Shy Guy(G)"
+    SHY_GUY_BK = "Shy Guy(Bk)"
+    DRY_BONES_GY = "Dry Bones(Gy)"
+    DRY_BONES_G = "Dry Bones(G)"
+    DRY_BONES_R = "Dry Bones(R)"
+    DRY_BONES_B = "Dry Bones(B)"
+    BRO_F = "Bro(F)"
+    BRO_B = "Bro(B)"
+
 
 images = {
     "Mario": "https://i.imgur.com/gX96BkX.png",
