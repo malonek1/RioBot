4975fb7f9fb4467cdbd398992a028f8952f90e20
Isadora Anemonae <IsadoraAnemonae@gmail.com>
2022-12-15 08:56:58 -0600

    Update EnvironmentVariables.py
    updating so that ideally this environment doesnt need to be touched for testing.

--- resources/EnvironmentVariables.py
+++ resources/EnvironmentVariables.py
@@ -1,7 +1,15 @@
 import json
+from os.path import exists
+
+prod_env = 'resources/prod.json'
+dev_env = 'resources/test.json'
 
 
 def get_var(var_input: str):
-    with open('resources/prod.json') as config:
+    env = prod_env
+    if exists(dev_env):
+        env = dev_env
+
+    with open(env) as config:
         env_vars = json.loads(config.read())
         return env_vars[var_input]
