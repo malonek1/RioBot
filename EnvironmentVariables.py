e4b13b4114e58bd20debd7ef26b7f57f9f660373
malonek1 <malonek997@gmail.com>
2022-12-14 22:50:38 -0500

    Switched from test back to prod

--- EnvironmentVariables.py
+++ EnvironmentVariables.py
@@ -2,7 +2,7 @@ import json
 
 
 def get_var(var_input: str):
-    with open('test.json') as config:
+    with open('prod.json') as config:
         env_vars = json.loads(config.read())
         return env_vars[var_input]
 
