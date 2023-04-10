db0f402434dc2984edde226f06f26ccc4938c10e
Pokebunny <pokebunnytaber@gmail.com>
2023-04-09 22:55:58 -0400

    submit command for rio web

--- RioBot.py
+++ RioBot.py
@@ -61,9 +61,8 @@ async def on_command_error(ctx, error):
             embed.add_field(name='Error:', value=str(error), inline=False)
             await ctx.send(embed=embed)
         else:
-            embed = discord.Embed(title='Please specify your score, your opponents score, and tag your opponent',
+            embed = discord.Embed(title='Command usage error',
                                   color=0xEA7D07)
-            embed.add_field(name='Example:', value='!submit 12 5 @user', inline=True)
             embed.add_field(name='Error:', value=str(error), inline=True)
             await ctx.send(embed=embed)
 
--- cogs/submit_results.py
+++ cogs/submit_results.py
@@ -4,53 +4,63 @@ import discord
 from discord.ext import commands
 import datetime as dt
 import pytz
+import requests
 
 from resources import SheetsParser
 from resources import EnvironmentVariables as ev
+from resources import ladders
 
 # Channel ID for #ranked-bot result submission channel
 RANKED_BOT_CHANNEL_ID = int(ev.get_var("ranked_bot_channel_id"))
 MOD_CHANNEL_ID = int(ev.get_var("mod_channel_id"))
 MOD_ROLE_ID = int(ev.get_var("mod_role_id"))
 
+WEB_SUBMIT_URL = "https://api.projectrio.app/submit_game"
+
+
 class SubmitResults(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     # Submit user command that allows a player to submit a game with another player
-    @commands.command(help = "type !submit <your score> <opponent score> @<your opponent>")
+    @commands.command(help="type !submit <game_id> <user1> <score1> <user2> <score2>")
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def submit(self, ctx, submitter_score: int, opp_score: int, opp_user: discord.Member):
+    async def submit(self, ctx, game_id: int, user1: str, score1: int, user2: str, score2: int):
         account_age = ctx.author.joined_at
         sysdate = dt.datetime.now(pytz.utc) - dt.timedelta(days=1)
         rb_channel = self.client.get_channel(RANKED_BOT_CHANNEL_ID)
         mod_channel = self.client.get_channel(MOD_CHANNEL_ID)
         if account_age < sysdate:
             if ctx.channel.id == RANKED_BOT_CHANNEL_ID:
+
                 # Check to make sure that runs values input by user are between 0 and 99
-                if (submitter_score < 0) or (opp_score < 0) or (submitter_score > 99) or (opp_score > 99):
+                if score1 < 0 or score2 < 0 or score1 > 99 or score2 > 99:
                     embed = discord.Embed(title='Scores must be between 0 and 100!', color=0xEA7D07)
                     await ctx.send(embed=embed)
                 else:
                     # Initial bot message displayed for game submitted by primary user
-                    submitter_user = ctx.author
-                    if submitter_user == opp_user:
+                    if user1 == user2:
                         embed = discord.Embed(title='You cannot submit a game against yourself!', color=0xEA7D07)
                         await ctx.send(embed=embed)
                     else:
+                        submitter_user = ctx.author
                         embed = discord.Embed(
-                            title='Are you submitting to the Stars-Off leaderboards or the Stars-On leaderboards?',
+                            title='What game mode are you submitting for?',
                             color=0xC496EF)
-                        embed.add_field(name='STARS-OFF', value=':goat:', inline=True)
-                        embed.add_field(name='STARS-ON', value=':star:', inline=True)
+                        embed.add_field(name=ladders.STARS_OFF_MODE, value=':goat:', inline=True)
+                        embed.add_field(name=ladders.STARS_ON_MODE, value=':star:', inline=True)
+                        embed.add_field(name=ladders.BIG_BALLA_MODE, value=':basketball:', inline=True)
                         game_mode_message = await ctx.send(embed=embed)
                         star_emoji = "\U00002B50"
                         goat_emoji = "\U0001F410"
+                        ball_emoji = "\U0001F3C0"
+                        emojis = [star_emoji, goat_emoji, ball_emoji]
                         await game_mode_message.add_reaction(goat_emoji)
                         await game_mode_message.add_reaction(star_emoji)
+                        await game_mode_message.add_reaction(ball_emoji)
 
                         def check_star(rxn, usr):
-                            return usr == submitter_user and (str(rxn.emoji) in [star_emoji, goat_emoji])
+                            return usr == submitter_user and (str(rxn.emoji) in emojis)
 
                         try:
                             reaction, user = await self.client.wait_for('reaction_add', timeout=300.0, check=check_star)
@@ -58,83 +68,55 @@ class SubmitResults(commands.Cog):
                             # If user doesn't react to message within 1 minute, initial message is deleted
                             await game_mode_message.delete()
                             embed = discord.Embed(
-                                title=f'Cancelled match between {submitter_user.name} and {opp_user.name} for not reacting in time!',
+                                title=f'Cancelled match between {user1} and {user2} for not reacting in time!',
                                 color=0xFF5733)
                             await ctx.send(embed=embed)
                         else:
-                            if reaction.emoji == star_emoji or reaction.emoji == goat_emoji:
+                            if reaction.emoji in emojis:
                                 if reaction.emoji == goat_emoji:
-                                    mode = 'OFF', ':goat:'
+                                    mode = ladders.STARS_OFF_MODE, ':goat:'
+                                elif reaction.emoji == ball_emoji:
+                                    mode = ladders.BIG_BALLA_MODE, ':basketball:'
                                 else:
-                                    mode = 'ON', ':star:'
+                                    mode = ladders.STARS_ON_MODE, ':star:'
+
+                                await game_mode_message.delete()
                                 embed = discord.Embed(
-                                    title=f'{opp_user.name}, confirm the results of your Stars-{mode[0]} {mode[1]} '
-                                          f'game by reacting with :white_check_mark: or reject the results with :x: ',
-                                    color=0xC496EF)
-                                embed.add_field(name=f'{submitter_user.name} score:', value=submitter_score, inline=True)
-                                embed.add_field(name=f'{opp_user.name} score:', value=opp_score, inline=True)
-                                confirm_message = await ctx.send(embed=embed)
-                                check_emoji = "\U00002705"
-                                ex_emoji = "\U0000274C"
-                                await confirm_message.add_reaction(check_emoji)
-                                await confirm_message.add_reaction(ex_emoji)
-
-                                # Check for bot to see if a user confirmed or denied the results submitted by another user
-                                def check_confirm(rxn, usr):
-                                    if usr == opp_user and (
-                                            str(rxn.emoji) in [check_emoji, ex_emoji]):
-                                        return usr == opp_user and (str(rxn.emoji) in [check_emoji, ex_emoji])
-                                    elif usr == submitter_user and (str(rxn.emoji) in [ex_emoji]):
-                                        return usr == submitter_user and (str(rxn.emoji) in [ex_emoji])
-
-                                try:
-                                    reaction, user = await self.client.wait_for('reaction_add', timeout=300.0,
-                                                                                check=check_confirm)
-                                except asyncio.TimeoutError:
-                                    # If user doesn't react to message within 5 minutes, initial message is deleted
-                                    await game_mode_message.delete()
-                                    await confirm_message.delete()
-                                    embed = discord.Embed(
-                                        title=f'Cancelled Stars-{mode[0]} match between {submitter_user.name} and {opp_user.name} for not reacting in time!',
-                                        color=0xFF5733)
-                                    await ctx.send(embed=embed)
+                                    title=f'Submitted {mode[0]} match between {user1} and {user2}!',
+                                    color=0x138F13)
+                                if score1 > score2:
+                                    winner_user = user1
+                                    winner_score = score1
+                                    loser_user = user2
+                                    loser_score = score2
                                 else:
-                                    # Confirmation message displays if secondary user reacts with check mark
-                                    if reaction.emoji == check_emoji:
-                                        await game_mode_message.delete()
-                                        await confirm_message.delete()
-                                        embed = discord.Embed(
-                                            title=f'Confirmed Stars-{mode[0]} match between {submitter_user.name} and {opp_user.name}!',
-                                            color=0x138F13)
-                                        await ctx.send(embed=embed)
-
-                                        # Update Spreadsheet
-                                        if submitter_score > opp_score:
-                                            print('Submitter Wins!')
-                                            SheetsParser.confirm_match(f'{submitter_user.name}', f'{opp_user.name}',
-                                                                       f'{submitter_user.id}', f'{opp_user.id}',
-                                                                       submitter_score,
-                                                                       opp_score, mode[0])
-                                        elif submitter_score < opp_score:
-                                            print('Opponent Wins!')
-                                            SheetsParser.confirm_match(f'{opp_user.name}', f'{submitter_user.name}',
-                                                                       f'{opp_user.id}', f'{submitter_user.id}', opp_score,
-                                                                       submitter_score, mode[0])
-                                    # Rejection message displays if secondary user reacts with an X mark
-                                    elif reaction.emoji == ex_emoji:
-                                        await game_mode_message.delete()
-                                        await confirm_message.delete()
-                                        embed = discord.Embed(
-                                            title=f'Cancelled Stars-{mode[0]} match between {submitter_user.name} and {opp_user.name}!',
-                                            color=0xFF5733)
-                                        await ctx.send(embed=embed)
+                                    winner_user = user2
+                                    winner_score = score2
+                                    loser_user = user1
+                                    loser_score = score2
+                                post_body = {
+                                    "GameID": game_id,
+                                    "Winner Username": winner_user,
+                                    "Winner Score": winner_score,
+                                    "Loser Username": loser_user,
+                                    "Loser Score": loser_score,
+                                    "TagSet": mode[0]
+                                }
+                                print(post_body)
+                                response = requests.post(WEB_SUBMIT_URL, json=post_body)
+                                print(response.status_code)
+                                print(response.json())
+                                await ctx.send(embed=embed)
 
             else:
                 embed = discord.Embed(color=0xEA7D07)
-                embed.add_field(name='The !submit command must be used here:', value='<#947699610921599006>')
+                embed.add_field(name='The !submit command must be used here:',
+                                value=f'<#{ev.get_var("ranked_bot_channel_id")}>')
                 await ctx.send(embed=embed)
         else:
-            print("User " + str(ctx.author.name) + " tried submitting a game with an invalid discord account age of " + str(account_age))
+            print("User " + str(
+                ctx.author.name) + " tried submitting a game with an invalid discord account age of " + str(
+                account_age))
             rb_embed = discord.Embed(
                 title=f"User {ctx.author.name} hasn't been in the server long enough to submit games!",
                 color=0xFF5733)
@@ -148,7 +130,8 @@ class SubmitResults(commands.Cog):
             mod_embed.add_field(name=f'Channel Activity:', value=f'<#{RANKED_BOT_CHANNEL_ID}>', inline=False)
             await rb_channel.send(embed=rb_embed)
             await mod_channel.send(f'<@&{MOD_ROLE_ID}>', embed=mod_embed)
-    @commands.command(help = "moderator tool to manually submit games")
+
+    @commands.command(help="moderator tool to manually submit games")
     @commands.has_any_role("Admins", "Moderator", "Bot Developer")
     async def forceSubmit(self, ctx, first_score: int, second_score: int, first_user: discord.Member,
                           second_user: discord.Member):
