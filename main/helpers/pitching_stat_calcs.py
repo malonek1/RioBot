cd113fe7ad9c1bbd245a1c5fe43bb6186131f4d2
Pokebunny <pokebunnytaber@gmail.com>
2023-05-10 13:39:40 -0400

    ostat code improvements

---
+++ main/helpers/pitching_stat_calcs.py
@@ -0,0 +1,19 @@
+import discord
+import requests
+
+from resources import characters
+
+async def pstat_user_char():
+    return
+
+
+async def pstat_user(ctx, user: str, mode: str):
+    return
+
+
+async def pstat_char():
+    return
+
+
+async def pstat_all():
+    return
