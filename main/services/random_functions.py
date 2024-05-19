eab67cba7c2f42191b50496e705bef4cc0f9de1b
Isadora Anemonae <IsadoraAnemonae@gmail.com>
2024-05-19 10:52:09 -0500

    Update random_functions.py
    Preemptive update for quickplay

--- main/services/random_functions.py
+++ main/services/random_functions.py
@@ -56,10 +56,11 @@ def rfRandomStadium():
 
 # Decide on a random mode to play and where
 def rfRandomMode():
-    random_modes = ["random teams"
-                    , "random balanced teams"
+    random_modes = ["Superstars Off"
+                    , "Superstars On"
                     , "BIG BALLA"
-                    , "Tee Ball"]
+                    , "Superstars Off Hazards"
+                    , "Remixed"]
     shuffle(random_modes)
     return "Play a game of " + random_modes.pop() + " at " + rfRandomStadium()
 
