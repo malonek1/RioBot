073e2587dbf758b82c26806a160b5dbbd67dd75f
Nick Taber <pokebunnytaber@gmail.com>
2026-05-19 15:18:48 -0400

    fix install scripts

--- scripts/run_service.sh
+++ scripts/run_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running RioBot"
 cd /home/ec2-user/RioBot/main
-sudo nohup python3.9 RioBot.py > /var/log/riobot.log 2>&1 &
+sudo nohup python3.11 RioBot.py > /var/log/riobot.log 2>&1 &
